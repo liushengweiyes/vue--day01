48f6375aa60a1b3d94545d0b7f0c6aaad4abed50
码农 <506298423@qq.com>
2018-01-03 21:54:47 +0800

    day01

---
+++ src/components/tabbar/MemberContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-container">
+    <h3>MemberContainer</h3>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+  .app-container{
+    padding-top: 40px;
+  }
+</style>
---
+++ src/components/tabbar/ShopcarContainer.vue
@@ -0,0 +1,15 @@
+<template>
+ <div class="app-container">
+    <h1>333</h1>
+ </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+  .app-container{
+    padding-top: 40px;
+  }
+</style>
